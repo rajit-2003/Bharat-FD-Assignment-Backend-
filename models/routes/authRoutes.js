1ffdfdb08663390015b653a32d2f7b0eae631204
stoinis <143214341+rajit-2003@users.noreply.github.com>
2025-02-02 02:44:20 +0530

    These are the routes for authentication (login, register).

--- models/routes/authRoutes.js
+++ models/routes/authRoutes.js
@@ -1,38 +1,11 @@
 // routes/authRoutes.js
 
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
-
+const { register, login } = require('../controllers/authController');
 const router = express.Router();
 
-// Register new user
-router.post('/register', async (req, res) => {
-  const { username, password, preferredLanguage } = req.body;
-
-  const user = new User({ username, password, preferredLanguage });
-
-  try {
-    await user.save(); // Save new user to database
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// Login user
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  const user = await User.findOne({ username });
-
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(400).json({ error: 'Invalid credentials' });
-  }
-
-  const token = jwt.sign({ id: user._id, preferredLanguage: user.preferredLanguage }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
-});
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
+
