9fb225d07f60cb73f6f0355c1a1aafaccac8e650
stoinis <143214341+rajit-2003@users.noreply.github.com>
2025-02-02 02:38:36 +0530

    Create authController.js

---
+++ models/controllers/authController.js
@@ -0,0 +1,32 @@
+// controllers/authController.js
+
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// User registration
+exports.register = async (req, res) => {
+  const { username, password, role } = req.body;
+  try {
+    const newUser = new User({ username, password, role });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered' });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// User login
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(400).json({ error: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
